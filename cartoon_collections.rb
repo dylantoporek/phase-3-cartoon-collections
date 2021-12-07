337882678634af6b414964fbe6c2a99aa068b8e5
DylanToporek <dylantoporek@gmail.com>
2021-12-07 13:29:00 -0500

    commit tree a7013a67c2290fa5acbfcfdb6cd5648e5c980bc3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  roll = array.each.with_index do |name, index|
+    index_to_display = index + 1
+    puts "#{index_to_display}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  response = array.map(&:capitalize) 
+  respnse_with_exclamation = response.map{|item| item + "!"}
+  respnse_with_exclamation
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  word_length = array.map{|word| word.length}
+  checker = word_length.any?{|num| num > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found_cheese = cheese_types.map{|cheese| array.find{|food| food == cheese}}
+  winner = found_cheese.first if !found_cheese.empty?
 end
